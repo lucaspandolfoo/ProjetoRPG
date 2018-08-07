7b7f2f394f94a20ffefb55dccce6b41001f700dc
guilherme.klafki <guilherme.klafki@LAB1-17-WIN.ROCHAS>
2018-08-07 19:52:53 -0300

    Criado a classe Oponente com o seus devidos atributos.

---
+++ ProjetoRPG/src/modelo/Oponente.java
@@ -0,0 +1,51 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package modelo;
+
+/**
+ *
+ * @author guilherme.klafki
+ */
+public class Oponente {
+    
+    private String nome;
+    private int vida;
+    private int ataque;
+    
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public int getVida() {
+        return vida;
+    }
+
+    public void setVida(int vida) {
+        this.vida = vida;
+    }
+
+    public int getAtaque() {
+        return ataque;
+    }
+
+    public void setAtaque(int ataque) {
+        this.ataque = ataque;
+    }
+
+    public String toString() {
+        return
+        "Atributos do Oponente: " + "\n" +
+        "Nome: " + nome + "\n" +
+        "Vida: " + vida + "\n" +
+        "Ataque: " + ataque + "\n";      
+    }
+    
+}
